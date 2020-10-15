37a56229b3e2b6bc839115b83001d86f913c7ed0
IvanMGM <ivanmgm88@gmail.com>
2020-10-16 00:22:53 +0200

    Legenda mas grande

--- Practica1/Script1.m
+++ Practica1/Script1.m
@@ -13,34 +13,34 @@
 %       --> Especificamos Funcion f(x)= e^(-x) + cos(4x/pi) que queremos 
 %           Aproximar mediante el Polinomio
 %           Interpolador de Lagrange. Esta funcion hay que especificarla dentro
-%           del Fichero de tipo Funcion?n llamado PIL.m. M?s adelante cuando trabajemos
-%           con Matlab simbolico quizas se pueda especificar desde aqu?.
+%           del Fichero de tipo Funcionon llamado PIL.m. M?s adelante cuando trabajemos
+%           con Matlab simbolico quizas se pueda especificar desde aqui.
 
 %       --> Especificamos el numero de puntos n discretos que utilizaremos
 %           para obtener el Polinomio Interpolador de Lagrange.
 %           Haremos un estudio para varios valores de n.
 %           Notar que en la teoria el valor de n NO se corresponde exactamente
 %           con el numero de puntos(en teoria n = numero Puntos - 1),
-%           pero en nuestro caso debido a las caracter?sticas internas de
-%           Matlab(Matlab indexa la primera posici?n de los vectores por el valor 1 y no el cero),
-%           el valor que toma n SI se corresponde con el n?mero de puntos 
-%           (en la pr?ctica n = n?mero de puntos).
+%           pero en nuestro caso debido a las caracteristicas internas de
+%           Matlab(Matlab indexa la primera posicion de los vectores por el valor 1 y no el cero),
+%           el valor que toma n SI se corresponde con el numero de puntos 
+%           (en la practica n = numero de puntos).
 %
 %       --> Aclaraciones: Lo que hace Matlab para dibujar es tomar dos
 %           puntos vecinos(entre medias no hay ningun otro punto) y unirlos
 %           mediante una recta. Al dibujar la funcion que queremos aproximar le
-%           decimos que dibuje un n?mero suficientemente grande de puntos (ej:1000) como
+%           decimos que dibuje un numero suficientemente grande de puntos (ej:1000) como
 %           para que a la vista del ojo humano, la linea recta sea lo
 %           suficientemente pequena como para no distinguirla, esto dota de curvatura 
 %           al dibujo en el caso de ser la funcion que queremos aproximar una
 %           funcion con cierta curvatura.
 
-%       --> Lo que se pretende con las Aproximaciones es utilizar un n?mero
+%       --> Lo que se pretende con las Aproximaciones es utilizar un numero
 %           mucho menor de puntos para obtener resultados 'suficientemente' buenos
-%           c?mo para poder utilizar la Aproximacion en lugar de la funci?n
+%           como para poder utilizar la Aproximacion en lugar de la funcion
 %           que estamos aproximando. De esta forma pasamos de evaluar la
-%           funcion real, por ejemplo, en 1000 puntos, a evaluar la Aproximaci?n de la
-%           funci?n real en solo 5 puntos obteniendo un error que, seg?n el
+%           funcion real, por ejemplo, en 1000 puntos, a evaluar la Aproximacion de la
+%           funcion real en solo 5 puntos obteniendo un error que, segun el
 %           contexto, sea lo suficientemente pequeno como para justificar la
 %           el uso de Aproximacion en sustitucion al uso de la funcion real
 %           por el Ahorro computacional que supone pasar de evaluar 1000
@@ -50,19 +50,15 @@
 %           consecuencia necesitaremos aproximaciones mejores, lo que trae
 %           coomo consecuencia la utilizacion de un mayor n?mero de
 %           puntos para obtener una Aproximacion lo suficientemente buena,
-%           y esto conllevar? un mayor coste computacional frente a una
-%           Aproximaci?n que haga uso de un menor numero de puntos.
-%           
-%           
-%           
-
+%           y esto conllevara un mayor coste computacional frente a una
+%           Aproximacion que haga uso de un menor numero de puntos.
 %
 %       --> En este caso al no estar utilizando Matlab simbolico, no tiene
 %           sentido calcular el Error cometido ya que los valores del Polinomio
 %           de Interprlacion de Lagrange particularizados en los puntos
 %           discretos del Intervalo cerrado [a,b] toman el mismo valor que la
 %           funcion f(x) que estamos aproximando, por lo que el error en esos
-%           puntos deria como resultado cero, lo interesante del error es
+%           puntos da como resultado cero, lo interesante del error es
 %           estudiarlo en puntos difrentes a los puntos discretos que hemos
 %           utilizado para obtener la aproximacion a traves del Polinimo de
 %           Interpolacion de Lagrange. Mas adelante, cuando obtengamos una
@@ -83,6 +79,7 @@
 %               suficientemente grande de puntos del Intervalo Cerrado [a,b] para
 %               utilizarlo como modelo de la funcion real en dicho
 %               intervalo.
+               format long;
                n=1000;
                X0=linspace(a,b,n);
                F=zeros(1,n);
@@ -104,7 +101,7 @@
                %Calculo el Error cometido |f(x)-I(x)|
                 F=zeros(1,n);
                 for i=1:n
-                    F(i)=exp(-X0(i)) + cos(4*X0(i)/pi);
+                    F(i)=exp(-X(i)) + cos(4*X(i)/pi);
                 end
                 E=abs(F - I);
                 CotaError1=max(E)
@@ -121,7 +118,7 @@
                %Calculo el Error cometido |f(x)-I(x)|
                 F=zeros(1,n);
                 for i=1:n
-                    F(i)=exp(-X0(i)) + cos(4*X0(i)/pi);
+                    F(i)=exp(-X(i)) + cos(4*X(i)/pi);
                 end
                 E=abs(F - I);
                 CotaError2=max(E)
@@ -136,7 +133,7 @@
                %Calculo el Error cometido |f(x)-I(x)|
                 F=zeros(1,n);
                 for i=1:n
-                    F(i)=exp(-X0(i)) + cos(4*X0(i)/pi);
+                    F(i)=exp(-X(i)) + cos(4*X(i)/pi);
                 end
                 E=abs(F - I);
                 CotaError3=max(E)
@@ -151,7 +148,7 @@
                %Calculo el Error cometido |f(x)-I(x)|
                 F=zeros(1,n);
                 for i=1:n
-                    F(i)=exp(-X0(i)) + cos(4*X0(i)/pi);
+                    F(i)=exp(-X(i)) + cos(4*X(i)/pi);
                 end
                 E=abs(F - I);
                 CotaError3=max(E)
@@ -166,7 +163,7 @@
                %Calculo el Error cometido |f(x)-I(x)|
                 F=zeros(1,n);
                 for i=1:n
-                    F(i)=exp(-X0(i)) + cos(4*X0(i)/pi);
+                    F(i)=exp(-X(i)) + cos(4*X(i)/pi);
                 end
                 E=abs(F - I);
                 CotaError5=max(E)
@@ -181,7 +178,7 @@
                %Calculo el Error cometido |f(x)-I(x)|
                 F=zeros(1,n);
                 for i=1:n
-                    F(i)=exp(-X0(i)) + cos(4*X0(i)/pi);
+                    F(i)=exp(-X(i)) + cos(4*X(i)/pi);
                 end
                 E=abs(F - I);
                 CotaError6=max(E)
@@ -190,8 +187,10 @@
                plot(X,I,'MarkerEdgeColor',[0.7 0.7 0.7],'LineWidth',2,'Marker','*','MarkerSize',6,'MarkerFaceColor',[0.7 0.7 0.7]); 
                legend('f(x) = exp(-x) + cos(4x/pi)','Aproximacion n=2','Aproximacion n=3',...
                'Aproximacion n=4','Aproximacion n=5','Aproximacion n=6','Aproximacion n=7'); 
+               lgd = legend;
+               lgd.FontSize = 18;
            
 %        
-%%
+
 
 
